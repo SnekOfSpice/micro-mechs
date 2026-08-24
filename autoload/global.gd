7a3740e1a78eb24bee8e5acbc46b1f7e4a45a2ca
Fio <leukert.fio@gmail.com>
2026-08-24 10:12:35 +0200

    epxort

--- autoload/global.gd
+++ autoload/global.gd
@@ -34,7 +34,16 @@ func get_uses_string(current, maximum) -> String:
 
 
 func get_weapon_configs() -> Array:
-	var result := []
-	for file in DirAccess.get_files_at("res://parts/weapons/configs/"):
-		result.append(file.trim_suffix(".tres"))
-	return result
+	return [
+		"cannon_1",
+		"cannon_2",
+		"cannon_3",
+		"shotgun",
+		"flamethrower",
+	]
+	
+	## vv this breaks in export
+	#var result := []
+	#for file in DirAccess.get_files_at("res://parts/weapons/configs/"):
+		#result.append(file.trim_suffix(".tres"))
+	#return result
