f7f4be6e59edac14e853802b49263430c72686ea
Fio <leukert.fio@gmail.com>
2026-09-04 08:50:13 +0200

    better camera

--- world/battle/camera_holder.gd
+++ world/battle/camera_holder.gd
@@ -1,15 +1,15 @@
-extends Camera3D
+extends Node3D
 
 
 
-#func _process(delta: float) -> void:
-	#if not Global.player_mech:
-		#return
-	#if not Global.npc_mech:
-		#return
-	##return
-	#var target_pos : Vector3 = Global.npc_mech.global_position.lerp(Global.player_mech.global_position, 0.5)
-	#global_position.z = global_position.move_toward(target_pos, delta * 400).z
+func _process(delta: float) -> void:
+	if not Global.player_mech:
+		return
+	if not Global.npc_mech:
+		return
+	#return
+	var target_pos : Vector3 = Global.npc_mech.global_position.lerp(Global.player_mech.global_position, 0.5)
+	global_position.z = global_position.move_toward(target_pos, delta * 400).z
 	#
 	#if not (Global.player_mech.is_entirely_on_screen()
 		#and Global.npc_mech.is_entirely_on_screen()):
